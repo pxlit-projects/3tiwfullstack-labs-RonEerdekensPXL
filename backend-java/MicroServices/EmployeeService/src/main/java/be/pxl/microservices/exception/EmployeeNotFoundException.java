4fc554061d2ea4574c2df207fc588824576a709e
RonEerdekensPXL <ron.eerdekens@student.pxl.be>
2024-09-26 15:38:27 +0200

    Fist test

--- backend-java/MicroServices/EmployeeService/src/main/java/be/pxl/microservices/exception/EmployeeNotFoundException.java
+++ backend-java/MicroServices/EmployeeService/src/main/java/be/pxl/microservices/exception/EmployeeNotFoundException.java
@@ -1,5 +1,9 @@
 package be.pxl.microservices.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Employee not found")
 public class EmployeeNotFoundException extends RuntimeException{
     public EmployeeNotFoundException() { }
     public EmployeeNotFoundException(String message) { super(message); }
--- backend-java/MicroServices/EmployeeService/src/test/java/be/pxl/microservices/EmployeeTests.java
+++ backend-java/MicroServices/EmployeeService/src/test/java/be/pxl/microservices/EmployeeTests.java
@@ -1,6 +1,66 @@
 package be.pxl.microservices;
 
+import be.pxl.microservices.domain.Employee;
+import be.pxl.microservices.repository.EmployeeRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 public class EmployeeTests {
 
+    @Autowired
+    MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Container
+    private static MySQLContainer sglContainer = new MySQLContainer(
+            "mysql:5.7.37"
+    );
+
+    @DynamicPropertySource
+    static void registerMySqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", sglContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", sglContainer::getUsername);
+        registry.add("spring.datasource.password", sglContainer::getPassword);
+    }
+
+    @Test
+    public void testCreateEmployee() throws Exception {
+        Employee employee = Employee.builder()
+                .age(24)
+                .name("Jan")
+                .position("student")
+                .build();
+
+        String employeeString = objectMapper.writeValueAsString(employee);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/employee")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(employeeString))
+                .andExpect(status().isCreated());
+
+        assertEquals(1,employeeRepository.findAll().size());
+    }
 }
